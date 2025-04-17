8bf704d79199cdceb7f18d61dcc91e48e09063c2
Ezequiel Abregu <ezequielabregu.code@gmail.com>
2025-04-17 12:55:18 -0300

    Fix app.run host

--- app.py
+++ app.py
@@ -126,5 +126,5 @@ import os
 print(f"Templates folder: {os.path.abspath('templates')}")
 
 if __name__ == '__main__':
-#    app.run(host="0.0.0.0", port=5000)    
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)    
+#    app.run(debug=True)
